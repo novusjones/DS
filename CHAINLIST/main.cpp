963ce9ad82b5daf2e04664d4853b11c5cf9ef23f
ZSY <2924653871@qq.com>
2026-05-07 16:12:41 +0800

    new file:   CHAINLIST/main.cpp

---
+++ CHAINLIST/main.cpp
@@ -0,0 +1,18 @@
+typedef struct DuLnode{
+    int data;
+    struct DuLnode *prior;
+    struct DuLnode *next;
+}DuLnode, *DuLinkList;//结构体指针是为了动态操做链表
+
+bool InitList(DuLinkList &L){ //传引用
+   L = new DuLnode; //创建头结点
+   if(L == nullptr) return false; //分配内存失败
+   L->prior = L->next = nullptr; //头结点的前驱和后继都指向空
+   return true;
+}
+
+int main(){
+    DuLinkList L; //现在还是野指针
+    InitList(L);
+    return 0;
+}
